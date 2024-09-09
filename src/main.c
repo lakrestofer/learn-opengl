7aa04105a0bc167403909d588f6232cfde0c8506
lakrestofer <lakrestofer@gmail.com>
2024-09-09 21:03:56 +0200

    make box rotate using tranformatio matrix

--- src/main.c
+++ src/main.c
@@ -1,11 +1,8 @@
 // system dependencies
 #include "glad/gl.h"
 #include "GLFW/glfw3.h"
-#include <cglm/affine.h>
-#include <cglm/mat4.h>
-#include <cglm/types.h>
-#include <stdbool.h>
 #include <cglm/cglm.h>
+#include <stdbool.h>
 // local dependencies
 #include "init.h"
 #include "shaders/utils.h"
@@ -24,58 +21,45 @@ void onResizeScreen(GLFWwindow* _, int width, int height) {
 // clang-format off
 float VERTICES[] = {
     // positions          // colors           // texture coords
-     0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   2.0f, 2.0f,   // top right
-     0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   2.0f, 0.0f,   // bottom right
+     0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
+     0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
     -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
-    -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 2.0f    // top left 
+    -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
 };
-unsigned int indices[] = {  // note that we start from 0!
-    0, 1, 3,   // first triangle
-    1, 2, 3    // second triangle
+unsigned int indices[] = {
+    0, 1, 3,
+    1, 2, 3
 }; 
 // does no transformation on the vertices
 const char *VERTEX_SHADER_SRC = GLSL(
     layout (location = 0) in vec3 aPos;
-    layout (location = 1) in vec3 aColor;
-    layout (location = 2) in vec2 aTexCoord;
-    out vec3 ourColor;
+    layout (location = 1) in vec2 aTexCoord;
+
     out vec2 TexCoord;
+
+    uniform mat4 transform;
     void main() {
-        gl_Position = vec4(aPos, 1.0);
-        ourColor = aColor;
+        gl_Position = transform * vec4(aPos, 1.0);
         TexCoord = aTexCoord;
     }
 );
 // colors each pixel within the triangle red
 const char *FRAGMENT_SHADER_SRC = GLSL(
     out vec4 FragColor;
-    in vec3 ourColor;
     in vec2 TexCoord;
     uniform sampler2D texture1;
-    uniform sampler2D texture2;
-    uniform float mixRatio;
     void main() {
-        FragColor = mix(texture(texture1, TexCoord), texture(texture2, vec2(1.0 - TexCoord.x, TexCoord.y)), mixRatio);
+        FragColor = texture(texture1, TexCoord);
     }    
 );
 // clang-format on
 
-void processInput(GLFWwindow* window, float* mixRatio, int mixRatioLoc) {
+void processInput(GLFWwindow* window) {
   if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
     glfwSetWindowShouldClose(window, true);
   }
-  if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) {
-    *mixRatio -= 0.01;
-    if (*mixRatio > 1.0) *mixRatio = 1.0;
-    if (*mixRatio < 0.0) *mixRatio = 0;
-    glUniform1f(mixRatioLoc, *mixRatio);
-  }
-  if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) {
-    *mixRatio += 0.01;
-    if (*mixRatio > 1.0) *mixRatio = 1.0;
-    if (*mixRatio < 0.0) *mixRatio = 0;
-    glUniform1f(mixRatioLoc, *mixRatio);
-  }
+  if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS) {}
+  if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS) {}
 }
 
 int main(void) {
@@ -84,8 +68,9 @@ int main(void) {
   GLFWwindow* w = initAndCreateWindow();
 
   // === register callbacks ==
-  glfwSetFramebufferSizeCallback(w, onResizeScreen); // register callback to run
-                                                     // when screen resizes
+  // register callback to run  when screen resizes
+  glfwSetFramebufferSizeCallback(w, onResizeScreen);
+
   // === compile and link shaders ==
   GLuint shaders[] = {
       initShader(GL_VERTEX_SHADER, VERTEX_SHADER_SRC),
@@ -98,7 +83,6 @@ int main(void) {
 
   // === load textures ===
   GLuint texture1 = createTexture("container.jpg", JPG);
-  GLuint texture2 = createTexture("awesomeface.png", PNG);
 
   // === setup gl objects ===
   GLuint VAO;       // vertex array object
@@ -124,43 +108,36 @@ int main(void) {
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
   glEnableVertexAttribArray(0);
   glVertexAttribPointer(
-      1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float))
+      1, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float))
   );
   glEnableVertexAttribArray(1);
-  glVertexAttribPointer(
-      2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float))
-  );
-  glEnableVertexAttribArray(2);
 
   // activate texture
   glActiveTexture(GL_TEXTURE0);
   glBindTexture(GL_TEXTURE_2D, texture1);
-  glActiveTexture(GL_TEXTURE1);
-  glBindTexture(GL_TEXTURE_2D, texture2);
 
   // tell opengl which texture unit each shader sampler belongs to
   glUseProgram(shader);
-  glUniform1i(glGetUniformLocation(shader, "texture1"), 0);
-  glUniform1i(glGetUniformLocation(shader, "texture2"), 1);
-
-  int mixRatioLoc = glGetUniformLocation(shader, "mixRatio");
-  float mixRatio = 0.5;
-  glUniform1f(mixRatioLoc, mixRatio);
+  GLuint shaderTextureLoc = glGetUniformLocation(shader, "texture1");
+  glUniform1i(shaderTextureLoc, 0);
 
   // == we setup our scaling,rotation,translation matrices
-  vec4 vec = {1.0f, 0.0f, 0.0f, 1.0f};
 
-  mat4 transformation = GLM_MAT4_IDENTITY;
-  glm_translate(transformation, (vec3){1.0, 1.0, 0.0});
-  glm_rotate(transformation, CGLM_PI_2, (vec3){0.0, 0.0, 1.0});
-  glm_scale(transformation, (vec3){0.5, 0.5, 0.5});
+  mat4 trans = GLM_MAT4_IDENTITY;
+  // glm_translate(trans, (vec3){0.3, 0.3, 0.0});
+  glm_scale(trans, (vec3){0.8, 0.8, 0.8});
 
-  vec4 dest;
-  glm_mat4_mulv(transformation, vec, dest);
+  GLuint transformLoc = glGetUniformLocation(shader, "transform");
+  glUniformMatrix4fv(transformLoc, 1, GL_FALSE, (GLfloat*)trans);
 
   // === Application loop ==
   while (!glfwWindowShouldClose(w)) {
-    processInput(w, &mixRatio, mixRatioLoc);
+    processInput(w);
+
+    mat4 transCopy;
+    glm_mat4_copy(trans, transCopy);
+    glm_rotate(transCopy, sin(glfwGetTime()) * 3.14, (vec3){0, 0, 1});
+    glUniformMatrix4fv(transformLoc, 1, GL_FALSE, (GLfloat*)transCopy);
 
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
