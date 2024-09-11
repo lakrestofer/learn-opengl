41e92e8d0f89eb086da27929d9650c2dbea12682
lakrestofer <lakrestofer@gmail.com>
2024-09-11 14:50:33 +0200

    fly mode camera

--- src/main.c
+++ src/main.c
@@ -6,6 +6,7 @@
 #include <cglm/mat4.h>
 #include <cglm/vec3.h>
 #include <stdbool.h>
+#include <string.h>
 // local dependencies
 #include "init.h"
 #include "shaders/utils.h"
@@ -17,13 +18,89 @@
 #define W 640
 #define WT "Hello World"
 
+// some aliases for functions to make them shorter
+#define onScreenResize glfwSetFramebufferSizeCallback
+#define onCursorEvent glfwSetCursorPosCallback
+
+// and aliases for constants
+#define VSHADER GL_VERTEX_SHADER
+#define FSHADER GL_FRAGMENT_SHADER
+
 typedef struct {
+  float yaw;
+  float pitch;
   vec3 pos;
-  vec3 up;
   vec3 front;
-  vec3 left;
-  vec4 right;
 } Camera;
+
+Camera defaultCamera(void) {
+  Camera c = {
+      .yaw = -90.0f,
+      .pitch = 0,
+      .pos = {0, 0, 3},
+      .front = {0, 0, -1},
+  };
+  return c;
+}
+/// generates the 'look at' matrix given current camera state
+void cameraLookAt(Camera* c, mat4 to) {
+  vec3 posFrontSum;
+  glm_vec3_add(c->pos, c->front, posFrontSum);
+  glm_lookat(c->pos, posFrontSum, (vec3){0, 1, 0}, to);
+}
+
+void moveCameraForward(Camera* c, float speed) {
+  vec3 direction;
+  glm_vec3_scale(c->front, speed, direction);
+  glm_vec3_add(c->pos, direction, c->pos);
+}
+void moveCameraBackward(Camera* c, float speed) {
+  vec3 direction;
+  glm_vec3_scale(c->front, speed, direction);
+  glm_vec3_sub(c->pos, direction, c->pos);
+}
+
+typedef struct {
+  float x;
+  float y;
+} Cursor;
+
+Cursor defaultCursorPos(void) {
+  Cursor p = {.x = W / 2, .y = H / 2};
+  return p;
+}
+
+typedef struct {
+  float delta_time;
+  float last_frame;
+} FrameTime;
+
+FrameTime defaultFrameTime(void) {
+  FrameTime t = {
+      .delta_time = 0.0f, // Time between current frame and last frame
+      .last_frame = 0.0f, // Time of last frame
+  };
+  return t;
+}
+void updateFrameTime(FrameTime* time, float now) {
+  time->delta_time = now - time->last_frame;
+  time->last_frame = now;
+}
+
+typedef struct {
+  Camera camera;
+  Cursor cursor;
+  FrameTime frame_t;
+} GameState;
+
+GameState defaultGameState(void) {
+  GameState state = {
+      .camera = defaultCamera(),
+      .cursor = defaultCursorPos(),
+      .frame_t = defaultFrameTime(),
+  };
+  return state;
+}
 
 // === callbacks ===
 
@@ -90,7 +167,7 @@ float vertices[] = {
     -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
 };
 // does no transformation on the vertices
-const char *VERTEX_SHADER_SRC = GLSL(
+const char *VSS = GLSL(
     layout (location = 0) in vec3 aPos;
     layout (location = 1) in vec2 aTexCoord;
 
@@ -107,7 +184,7 @@ const char *VERTEX_SHADER_SRC = GLSL(
     }
 );
 // colors each pixel within the triangle red
-const char *FRAGMENT_SHADER_SRC = GLSL(
+const char *FSS = GLSL(
     out vec4 FragColor;
     in vec2 TexCoord;
     uniform sampler2D texture1;
@@ -117,45 +194,59 @@ const char *FRAGMENT_SHADER_SRC = GLSL(
 );
 // clang-format on
 
-#define CAMERA_SPEED 0.05f
+#define closeWindow() glfwSetWindowShouldClose(w, true);
+#define K(key) GLFW_KEY_##key
+#define keyPressed(key) ((glfwGetKey(w, K(key))) == (GLFW_PRESS))
+void handleInput(GLFWwindow* w, GameState* s) {
+  float dt = s->frame_t.delta_time;
 
-void processInput(GLFWwindow* window, Camera* camera) {
-  vec3 diff;
-  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
-    glfwSetWindowShouldClose(window, true);
+  if (keyPressed(ESCAPE)) closeWindow();
+  if (keyPressed(UP)) moveCameraForward(&s->camera, 2.5f * dt);
+  if (keyPressed(DOWN)) moveCameraBackward(&s->camera, 2.5f * dt);
+}
+
+bool firstMouse = true;
+void mouse_callback(GLFWwindow* w, double cx, double cy) {
+  GameState* s = glfwGetWindowUserPointer(w);
+  if (!s) return;
+
+  if (firstMouse) {
+    s->cursor.x = cx;
+    s->cursor.y = cy;
+    firstMouse = false;
   }
-  if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) {
-    glm_vec3_scale(camera->front, CAMERA_SPEED, diff);
-    glm_vec3_add(camera->pos, diff, camera->pos);
-  }
-  if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) {
-    glm_vec3_scale(camera->front, -CAMERA_SPEED, diff);
-    glm_vec3_add(camera->pos, diff, camera->pos);
-  }
-  if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS) {
-    glm_vec3_scale(camera->left, CAMERA_SPEED, diff);
-    glm_vec3_add(camera->pos, diff, camera->pos);
-  }
-  if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS) {
-    glm_vec3_scale(camera->right, CAMERA_SPEED, diff);
-    glm_vec3_add(camera->pos, diff, camera->pos);
-  }
+
+  double dx = (cx - s->cursor.x);
+  double dy = (s->cursor.y - cy);
+  s->cursor.x = cx;
+  s->cursor.y = cy;
+  s->camera.yaw += dx * 0.2f;
+  s->camera.pitch += dy * 0.2f;
+  if (s->camera.pitch > 89.0f) s->camera.pitch = 89.0f;
+  if (s->camera.pitch < -89.0f) s->camera.pitch = -89.0f;
+  vec3 dir = {
+      cos(glm_rad(s->camera.yaw)) * cos(glm_rad(s->camera.pitch)),
+      sin(glm_rad(s->camera.pitch)),
+      sin(glm_rad(s->camera.yaw)) * cos(glm_rad(s->camera.pitch)),
+  };
+  glm_normalize(dir);
+  memcpy(s->camera.front, dir, sizeof(vec3));
 }
 
 int main(void) {
-
   // === Init glfw and gl context ===
   GLFWwindow* w = initAndCreateWindow();
 
+  // === set glfw settings ===
+  glfwSetInputMode(w, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
   // === register callbacks ==
   // register callback to run  when screen resizes
-  glfwSetFramebufferSizeCallback(w, onResizeScreen);
+  onScreenResize(w, onResizeScreen);
+  onCursorEvent(w, mouse_callback);
 
   // === compile and link shaders ==
-  GLuint shaders[] = {
-      initShader(GL_VERTEX_SHADER, VERTEX_SHADER_SRC),
-      initShader(GL_FRAGMENT_SHADER, FRAGMENT_SHADER_SRC),
-  };
+  GLuint shaders[2] = {initShader(VSHADER, VSS), initShader(FSHADER, FSS)};
   if (!shaderIsValid(shaders[0]) || !shaderIsValid(shaders[1])) goto clean;
   GLuint shader = linkShaders(shaders, 2);
   if (!shaderProgramIsValid(shader)) goto clean;
@@ -175,10 +266,8 @@ int main(void) {
 
   // setup how to interpret the buffer data
   glBindVertexArray(VAO);
-  // bind texture
   glBindTexture(GL_TEXTURE_2D, texture1);
-  // copy vertex data from ram to vram
-  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO); // ram -> vram
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
   // set vertex attribute pointers
   // position attribute
@@ -195,26 +284,17 @@ int main(void) {
 
   // tell opengl which texture unit each shader sampler belongs to
   glUseProgram(shader);
-  GLuint shaderTextureLoc = glGetUniformLocation(shader, "texture1");
-  glUniform1i(shaderTextureLoc, 0);
+  glUniform1i(glGetUniformLocation(shader, "texture1"), 0);
 
-  // == we setup our scaling,rotation,translation matrices
-
-  // camera
-  Camera camera = {.pos = {0, 0, 3}, .front = {0, 0, -1}, .up = {0, 1, 0}};
-  glm_cross(camera.front, camera.up, camera.right);
-  glm_vec3_normalize(camera.right);
-  glm_vec3_negate_to(camera.right, camera.left);
+  // == we setup global game state ===
+  GameState state = defaultGameState();
 
   // model view projection matrices
   mat4 model = GLM_MAT4_IDENTITY;
-  glm_rotate(model, glm_rad(-55.0f), (vec3){1, 0, 0});
-  mat4 view;
-  vec3 cameraPos2;
-  glm_vec3_add(camera.pos, camera.front, cameraPos2);
-  glm_lookat(camera.pos, cameraPos2, camera.up, view);
-
+  mat4 view = GLM_MAT4_IDENTITY;
   mat4 projection = GLM_MAT4_IDENTITY;
+  glm_rotate(model, glm_rad(-55.0f), (vec3){1, 0, 0});
+  cameraLookAt(&state.camera, view);
   glm_perspective(glm_rad(45.0), W / H, 0.1, 100.0, projection);
 
   GLuint modelLoc = glGetUniformLocation(shader, "model");
@@ -225,10 +305,18 @@ int main(void) {
   glUniformMatrix4fv(viewLoc, 1, GL_FALSE, (float*)view);
   glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, (float*)projection);
 
+  // we make game state available from everywhere
+  glfwSetWindowUserPointer(w, &state);
+
   // === Application loop ==
   while (!glfwWindowShouldClose(w)) {
-    processInput(w, &camera);
+    // update
+    updateFrameTime(&state.frame_t, glfwGetTime());
 
+    // handle input
+    handleInput(w, &state);
+
+    // draw
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
@@ -236,16 +324,13 @@ int main(void) {
     glUseProgram(shader);
 
     glBindVertexArray(VAO);
-
-    mat4 localView;
-    glm_vec3_add(camera.pos, camera.front, cameraPos2);
-    glm_lookat(camera.pos, cameraPos2, camera.up, localView);
-    glUniformMatrix4fv(viewLoc, 1, GL_FALSE, (float*)localView);
+    cameraLookAt(&state.camera, view); // update view matrix from camera
+    glUniformMatrix4fv(viewLoc, 1, GL_FALSE, (float*)view);
 
     for (int i = 0; i < 10; i++) {
+      float angle = 20.0f * i;
       mat4 localModel = GLM_MAT4_IDENTITY;
       glm_translate(localModel, cubePositions[i]);
-      float angle = 20.0f * i;
       glm_rotate(model, glm_rad(angle), (vec3){1.0f, 0.3f, 0.5f});
       glUniformMatrix4fv(modelLoc, 1, GL_FALSE, (float*)localModel);
       glDrawArrays(GL_TRIANGLES, 0, 36);
